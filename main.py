8c9de214e0102ab5903da0d25cc1e29c03b7d9ba
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-05-08 10:29:31 +0530

    Fixed some errors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-    try:
-        return render_template('home.html')
+    return render_template('home.html')
